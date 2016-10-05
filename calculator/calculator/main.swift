ae893be5ed09026c775f7cc4c5580f6eb58e2dbf
Austin Weale <austinweale@gmail.com>
2016-10-04 19:42:54 -0700

    test again

--- calculator/calculator/main.swift
+++ calculator/calculator/main.swift
@@ -1,14 +1,12 @@
 //
-//  main.swift
+//  Austin Weale
 //  calculator
 //
-//  Created by iGuest on 10/4/16.
-//  Copyright © 2016 iGuest. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+print("Enter an expression separated by returns:")
 
 var first = Int(readLine(strippingNewline: true)!)!
 
