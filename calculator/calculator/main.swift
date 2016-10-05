8977978e06d39215f9e5dfeaa0dc4b02a00c278f
Austin Weale <austinweale@gmail.com>
2016-10-05 12:54:41 -0700

    command line arguments work

--- calculator/calculator/main.swift
+++ calculator/calculator/main.swift
@@ -6,14 +6,25 @@
 
 import Foundation
 
-class OperatorHelp {
-    func average(args: [String]) {
-        let total = args.count - 2
-        var commandLineSum = 0
-        for i in 1...(args.count - 2){
-            commandLineSum += Int(args[i])!
-        }
-        print("result: \(commandLineSum / total)")
+
+//returns the average of the command-line arguments
+//assumes first arg is the program name and the last arg is not used
+func average(args: [String]) {
+    let total = args.count - 2
+    var commandLineSum = 0
+    for i in 1...(args.count - 2){
+        commandLineSum += Int(args[i])!
+    }
+
+    print("result: \(commandLineSum / total)")
+}
+
+//returns the factorial of the passed-in integer
+func factorial(num: Int) -> Int{
+    if num == 1 {
+        return 1
+    }else{
+        return num * factorial(num: num - 1)
     }
 }
 
@@ -21,10 +32,13 @@ class OperatorHelp {
 if (CommandLine.arguments.count > 1){
     var arguments = CommandLine.arguments
     var operation = arguments[arguments.count - 1]
-    let operatorHelp = OperatorHelp()
     switch operation {
         case "avg":
-            operatorHelp.average(args: arguments)
+            average(args: arguments)
+        case "count":
+            print("result: \(arguments.count - 2)")
+        case "fact":
+            print("result: \( factorial(num: Int(arguments[1])!) )")
         default:
             print("invalid operation")
     }
